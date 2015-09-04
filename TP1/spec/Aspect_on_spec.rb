bc8f4902d38c66bdad2ca9cd48a70806bee00d02
ezebor <ezequiel.borenstein@gmail.com>
2015-09-04 19:44:32 -0300

    testiado el mensaje on de Aspect

--- TP1/spec/Aspect_on_spec.rb
+++ TP1/spec/Aspect_on_spec.rb
@@ -1,5 +1,5 @@
 require 'rspec'
-require_relative '../src/aspect'
+require_relative '../src/Aspect'
 
 #estructuras para testear
 
@@ -12,19 +12,34 @@ end
 module M
 end
 
+arreglo = Array.new
+
+Aspect.on Moto, arreglo, M, /^S/ do
+  where
+end
+
 describe 'Testeo que el mensaje on funcione para instancias, clases, modulos y expresiones regulares' do
 
-  let(:arreglo) {
-    Array.new
-  }
-
-  it 'Cualquier instancia de Array deberia entender el mensaje nuevo_metodo' do
-
-      Aspect.on Auto {where}
-      expect(Auto.instance_methods.include? :nuevo_metodo).to eq(true)
-
+  it 'Cualquier instancia de Moto deberia entender el mensaje nuevo_metodo' do
+      expect(Moto.instance_methods.include? :nuevo_metodo).to eq(true)
   end
 
+  it 'La instancia arreglo deberia entender el mensaje nuevo_metodo' do
+    expect(arreglo.methods.include? :nuevo_metodo).to eq(true)
+  end
+
+  it 'Cualquier instancia de Array no deberia entender el mensaje nuevo_metodo' do
+    expect(Array.new.methods.include? :nuevo_metodo).to eq(false)
+  end
+
+  it 'Cualquier instancia de String deberia entender el mensaje nuevo_metodo por la regex /^S/' do
+    expect(String.instance_methods.include? :nuevo_metodo).to eq(true)
+  end
+
+  it 'Cualquier instancia de Auto deberia entender el mensaje nuevo_metodo si incluye el modulo M' do
+    Auto.include(M)
+    expect(Auto.instance_methods.include? :nuevo_metodo).to eq(true)
+  end
 
 end
 
