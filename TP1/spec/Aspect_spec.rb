5767cc019ceea7ff46747cae52a89441773424b3
JuanchiRios <juanchirios04@hotmail.com>
2015-09-04 12:55:16 -0300

    subo los test mas basicos de select_origins()

--- TP1/spec/Aspect_spec.rb
+++ TP1/spec/Aspect_spec.rb
@@ -17,10 +17,10 @@ module B_test
 
 end
 
-@pepita_test = Pepita_test.new
-@pepe_test = Pepe_test.new
+pepita_test = Pepita_test.new
+pepe_test = Pepe_test.new
 
-describe 'Aspect basic tests' do
+describe 'Test basicos de Aspect con clases, modules y instancias por separado' do
   let(:expected_origins) {
     Array.new
   }
@@ -30,36 +30,15 @@ describe 'Aspect basic tests' do
 
     expect(Aspect.select_origins(Pepe_test,Pepita_test)).to eq(expected_origins)
   end
-=begin
+
   it 'deberia devolver mixins cuando le mando solo mixins' do
-    expected_origins.push(Pepe_test,Pepita_test)
-    expect(Aspect.select_origins(A_test,B_test,{})).to eq(expected_origins)
+    expected_origins.push(A_test,B_test)
+    expect(Aspect.select_origins(A_test,B_test)).to eq(expected_origins)
   end
 
   it 'deberia devolver unos objetos cuando le mando solo unos objetos' do
-
+    expected_origins.push(pepita_test,pepe_test)
+    expect(Aspect.select_origins(pepita_test,pepe_test)).to eq(expected_origins)
   end
-
-end
-
-=begin
-  let(:una_golondrina) {
-    Golondrina.new
-  }
-
-
-
-  it 'deberia perder energia cuando vuelva' do
-    una_golondrina.volar(10)
-    expect(una_golondrina.energia).to eq(900)
-  end
-
-  it 'deberia ganar energia cuando come' do
-    una_golondrina.comer(5)
-    expect(una_golondrina.energia).to eq(1025)
-
-  end
-
-end
-=end
+  
 end
