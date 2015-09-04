2d127f0d48ed8344b5452c013137a33420553357
JuanchiRios <juanchirios04@hotmail.com>
2015-09-04 12:34:48 -0300

    extraje el metodo select_origins y agregue un test sobre ese metodo

--- TP1/spec/Aspect_spec.rb
+++ TP1/spec/Aspect_spec.rb
@@ -1,8 +1,46 @@
 require 'rspec'
 require_relative '../src/Aspect'
-=begin
-describe 'Aspect' do
 
+class Pepita_test
+
+end
+
+class Pepe_test
+
+end
+
+module A_test
+
+end
+
+module B_test
+
+end
+
+@pepita_test = Pepita_test.new
+@pepe_test = Pepe_test.new
+
+describe 'Aspect basic tests' do
+  let(:expected_origins) {
+    Array.new
+  }
+
+  it 'deberia devolver las clases cuando le mando solo clases' do
+    expected_origins.push(Pepe_test,Pepita_test)
+  expect(Aspect.select_origins(Pepe_test,Pepita_test,{})).to eq(expected_origins)
+  end
+=begin
+  it 'deberia devolver mixins cuando le mando solo mixins' do
+
+  end
+
+  it 'deberia devolver unos objetos cuando le mando solo unos objetos' do
+
+  end
+
+end
+
+=begin
   let(:una_golondrina) {
     Golondrina.new
   }
@@ -22,3 +60,4 @@ describe 'Aspect' do
 
 end
 =end
+end
