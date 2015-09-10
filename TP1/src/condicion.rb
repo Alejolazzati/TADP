daf912a917ff373bc01c9afca6dc356dfc860fb3
JuanchiRios <juanchirios04@hotmail.com>
2015-09-10 12:21:11 -0300

    agrego un TODO

--- TP1/src/condicion.rb
+++ TP1/src/condicion.rb
@@ -17,13 +17,13 @@ module Condicion
   end
 
   def has_parameters(cant, arg_type = optional_mandatory)
-    condition = arg_type.instance_of?(Regexp) ?  type_regex(arg_type) : arg_type
+    condition = arg_type.instance_of?(Regexp) ?  type_regex(arg_type) : arg_type##TODO name(arg_type) en vez de type_regex(arg_type)
     proc {|a_method| instance_method(a_method).parameters.count &condition == cant}
 
   end
 
   def type_regex(regex)
-    proc{|_,nom|nom =~ regex }
+    proc{|_,nom|nom. =~ regex }
   end
   def mandatory
     proc {|mode,_| mode == :req }
