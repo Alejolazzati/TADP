9c75d5d548dd8f6f55219bdeff737ea2a52b0433
JuanchiRios <juanchirios04@hotmail.com>
2015-09-26 09:27:19 -0300

    cambio inject

--- TP1/src/transformacion.rb
+++ TP1/src/transformacion.rb
@@ -18,36 +18,25 @@ class Transformacion
 
   def redirect_to(objetivo)
     real_method = @real_method.name
-    @metodo = proc {|*args|objetivo.send(real_method, *args)}
+    @metodo = proc { |*args| objetivo.send(real_method, *args) }
   end
 
   def instead_of(&nuevo_metodo)
-    @metodo = proc {|*args|instance_exec(self, *args, &nuevo_metodo)}
+    @metodo = proc { |*args| instance_exec(self, *args, &nuevo_metodo) }
   end
 
   def after(&after)
     metodo = @metodo
-    @metodo =proc {|*args|
-      metodo1 =  metodo.is_a?(UnboundMethod) ? metodo.bind(self) : metodo
+    @metodo =proc { |*args|
+      metodo1 = metodo.is_a?(UnboundMethod) ? metodo.bind(self) : metodo
       instance_exec(*args, &metodo1)
-      instance_exec(self, *args, &after)}
+      instance_exec(self, *args, &after) }
   end
 
-=begin
-  def after(&after)
-    metodo = @metodo
-    @metodo = proc { |*args|
-      metodo1 =  metodo.is_a?(UnboundMethod) ? metodo.bind(self) : metodo
-      instance_exec(*args, &metodo1)
-      instance_exec(self, *args, &after)
-    }
-  end
-=end
   def before(&before)
     metodo = @metodo
     @metodo = proc { |*parametros|
-     metodo1 =  metodo.is_a?(UnboundMethod) ? metodo.bind(self) : metodo
-
+      metodo1 = metodo.is_a?(UnboundMethod) ? metodo.bind(self) : metodo
       continuacion = proc { |instancia, cont, *new_parameters| instance_exec(*new_parameters, &metodo1) } #REVISAR
       instance_exec(self, continuacion, *parametros, &before) }
   end
@@ -56,26 +45,25 @@ class Transformacion
     parametros = @real_method.parameters.map { |_, sym| sym }
     new_args= hasht.map { |key, value| [get_index(parametros, key), value] }
     method_name = @real_method.name
-    method=  @metodo
-    before do |instance, cont, *args|
-      metodo1 =  method.is_a?(UnboundMethod) ? method.bind(self) : method
+    method= @metodo
+    @metodo =  proc {|*args|
+        metodo1 = method.is_a?(UnboundMethod) ? method.bind(self) : method
 
-      new_args.each do |index, value|
-        old_value = args[index]
-        args[index] = value.is_a?(Proc) ? value.call(self, method_name, old_value) : value
-      end
-      instance_exec(*args, &metodo1)
-#      cont.call(self, method, *args)
+        new_args.each do |index, value|
+          old_value = args[index]
+          args[index] = value.is_a?(Proc) ? value.call(self, method_name, old_value) : value
+        end
+        instance_exec(*args, &metodo1)
+      }
     end
-  end
 
-  def get_index(parameters, key)
-    index = parameters.find_index { |parameter| parameter == key }
-    index.nil? ? (raise ArgumentError.new 'Ese parametro no existe PAPA!') : index
-  end
+    def get_index(parameters, key)
+      index = parameters.find_index { |parameter| parameter == key }
+      index.nil? ? (raise ArgumentError.new 'Ese parametro no existe PAPA!') : index
+    end
 
-  def get_value(value, original_parameter, method_name)
-    value.is_a?(Proc) ? value.call(self, method_name, original_parameter) : value
-  end
+    def get_value(value, original_parameter, method_name)
+      value.is_a?(Proc) ? value.call(self, method_name, original_parameter) : value
+    end
 
-end
+  end
