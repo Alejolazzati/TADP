ec2468a87ecd5caf0968439f23c5eec05990db9c
JuanchiRios <juanchirios04@hotmail.com>
2015-09-26 03:08:25 -0300

    wax off

--- TP1/src/transformacion.rb
+++ TP1/src/transformacion.rb
@@ -8,34 +8,13 @@ class Transformacion
 
   def transformate(&bloquesin)
     instance_eval &bloquesin
-
     metodo = @metodo
     real_method = @real_method
-    #   after = @after
-    #  before = @before
-    #   inject = @inject
     @target.send(:define_method, real_method.name) do
     |*parametros|
-      #     metodo = metodo.unbind if metodo.is_a?(Method)
-      #    metodo = metodo.bind(self) if metodo.is_a?(UnboundMethod)
       metodo1 =  metodo.is_a?(UnboundMethod) ? metodo.bind : metodo
-
-=begin
-      inject.each do |index, value|
-        old_value = parametros[index]
-        parametros[index] = value.is_a?(Proc) ? value.call(self, real_method.name, old_value) : value
-      end
-=end
-
       instance_exec(*parametros, &metodo1)
-      #      sin_after = before.nil? ? instance_exec(*parametros, &metodo)
-      #        : instance_exec(*parametros, &before)
-
-      #      resultado =after.nil? ? sin_after : instance_exec(*parametros, &after)
-      #    metodo = metodo.unbind if metodo.is_a?(Method) #para que no tenga efecto de lado haciendolo varias veces
-      #     resultado
     end
-
   end
 
   def redirect_to(objetivo)
@@ -62,21 +41,14 @@ class Transformacion
   def before(&before)
     metodo = @metodo
     @metodo = proc { |*parametros|
-
+#     metodo1 =  metodo.is_a?(UnboundMethod) ? metodo.bind : metodo
  #     metodo = metodo.unbind if metodo.is_a?(Method)
      metodo = metodo.bind(self) if metodo.is_a?(UnboundMethod)
-#     metodo1 =  metodo.is_a?(UnboundMethod) ? metodo.bind : metodo
+
       continuacion = proc { |instancia, cont, *new_parameters| metodo.call(*new_parameters) } #REVISAR
       instance_exec(self, continuacion, *parametros, &before) }
   end
 
-=begin
-  before do |instance, cont, *args|
-    @x = 10
-    new_args = args.map{ |arg| arg * 10 }
-    cont.call(self, nil, *new_args)
-  end
-=end
   def inject(hasht)
     parametros = @real_method.parameters.map { |_, sym| sym }
     new_args= hasht.map { |key, value| [get_index(parametros, key), value] }
